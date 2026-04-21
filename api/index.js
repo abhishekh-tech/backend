a16e36b011b1e07a0d921e4dfc6f6c8bd5d12b56
unknown <gabhishekh1@gmail.com>
2026-04-21 08:09:12 +0530

    Fix CORS by allowing wildcard origin

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
   await connectToDatabase();
 
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
