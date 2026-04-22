8173a69bc8f8de8d8789495eccddd0e8ba13083f
unknown <gabhishekh1@gmail.com>
2026-04-22 09:01:50 +0530

    frontend URL changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://frontend-f63r-jrs5mox87-abhishekh-redmen-team.vercel.app',
+  origin: ['https://frontend-f63r.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
